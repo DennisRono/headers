cf0d331cc424fb5b3aaebff8b34c7d6d41d89c4b
DennisRono <finnneron1@gmail.com>
2022-04-05 16:48:30 +0300

    pass card api reg

--- header_two/js/main.js
+++ header_two/js/main.js
@@ -3,17 +3,17 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let slides = document.getElementsByClassName("test-soc-slide");
+  let dots = document.getElementsByClassName("test-author-one");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" activeAuthor", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  dots[slideIndex-1].className += " activeAuthor";
+  setTimeout(showSlides, 5000);
 }
